e2d5ec888341be537b4e7d52442e0aa7c981af11
Ben <menubar@protonmail.com>
2018-07-18 17:16:06 -0400

    new

--- handler.js
+++ handler.js
@@ -1,5 +1,6 @@
 const { promisify } = require('util')
-const { lookup } = require('whois-2')
+const whois = require('whois-2')
+const parse = require('./parse')
 
 const createClient = async () => {
   const client = require('redis')
@@ -17,7 +18,9 @@ const createClient = async () => {
   client.on('end', () => console.log('end'))
   return {
     get: promisify(client.get).bind(client),
-    set: promisify(client.set).bind(client),
+    set: (key, value) => new Promise((resolve, reject) =>
+      client.set(key, value, 'EX', 60 * 60 * 24 * 1000, resolve)
+    ), 
     end: () => new Promise((resolve, reject) => {
       client.on('end', resolve)
       client.quit()
@@ -35,17 +38,34 @@ const respond = (message = {}) => ({
   }
 })
 
+const lookup = (id, { client }) => new Promise((resolve, reject) => 
+  whois.lookup(id, async (err, whoisData) => {
+    whoisData = parse(whoisData)
+    whoisData.available = Object.keys(whoisData).length
+      ? false
+      : true
+
+    client.set(id, JSON.stringify(whoisData))
+    resolve(whoisData)
+  })
+)
+
 module.exports.handler = async (event, context, callback) => {
   const client = await createClient()
   const { id } = event.pathParameters
   let data = await client.get(id)
+  if (data) {
+    data = JSON.parse(data)
+    if (data.registry_expiry_date) {
+      let expiry = new Date(data.registry_expiry_date).getTime()
+      if (Date.now() + (24 * 60 * 60 * 1000) > expiry) {
+        data = null
+      }
+    }
+  }
+
   if (!data) {
-    data = await new Promise((resolve, reject) => 
-      lookup(id, async (err, info) => {
-        await client.set(id, data)
-        resolve(info)
-      })
-    )
+    data = await lookup(id, { client })
   }
 
   await client.end()
